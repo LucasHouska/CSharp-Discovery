13cadda3c7fa7aabd78fbcc0abf493ff237e9d38
Lucas Houska <ljhouska75@gmail.com>
2022-05-02 16:28:42 -0500

    started C#!

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+int myNum = 7;
+long longNum = 1000000000000000000L;
+float halfMyNumber = 3.5F;
+double halfMyNumberWithDouble = 3.50000D;
+bool isCSharpFun = true;
+bool isFishTasty = false;
+char myFirstInitial = 'L';
+string hello = "hello";
+
+Console.WriteLine(hello);
+Console.WriteLine(myNum);
+Console.WriteLine(longNum);
+Console.WriteLine(halfMyNumber);
+Console.WriteLine(halfMyNumberWithDouble);
+Console.WriteLine(isCSharpFun);
+Console.WriteLine(isFishTasty);
+Console.WriteLine(myFirstInitial);
